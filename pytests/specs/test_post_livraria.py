d8b01a05cc8ee32866a1702f5fd72308e1db0352
Patrick de Alcantara Strogueia <patrickdealcantara@gmail.com>
2025-07-21 23:42:53 -0300

    Aula 14

---
+++ pytests/specs/test_post_livraria.py
@@ -0,0 +1,9 @@
+import pytest
+from pytests.mocks.livraria_mock import *
+from pytests.clients.post_livraria_client import PostLivrariaClient
+
+
+@pytest.mark.crud_livros
+def test_post_livro():
+    payload = payload_post_livros()
+    PostLivrariaClient.post_livros(payload)
