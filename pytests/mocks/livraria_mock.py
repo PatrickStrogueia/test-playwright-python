f4690f93db7d9cdfec3bc3b87bc8890ba596271f
Patrick de Alcantara Strogueia <patrickdealcantara@gmail.com>
2025-07-23 23:52:18 -0300

    Aula 19

--- pytests/mocks/livraria_mock.py
+++ pytests/mocks/livraria_mock.py
@@ -1,7 +1,7 @@
 def payload_post_livros():
     return {
         "nome": "Test Playwright + Python",
-        "auto": "Patrick",
+        "autor": "Patrick",
         "dataPublicacao": "2025-07-21",
         "qtdePaginas": 200,
     }
