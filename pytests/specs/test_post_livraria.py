8b7ef70f031b940ba061e4c30f070902436d821c
Patrick de Alcantara Strogueia <patrickdealcantara@gmail.com>
2025-07-24 22:59:31 -0300

    Aula 20

--- pytests/specs/test_post_livraria.py
+++ pytests/specs/test_post_livraria.py
@@ -2,6 +2,7 @@ from pytests.support.hooks import *
 from pytests.mocks.livraria_mock import *
 from pytests.clients.post_livraria_client import PostLivrariaClient
 from pytests.support.api_utils import ApiUtils
+from pytests.schemas.post_livros_schema import *
 
 
 @pytest.mark.crud_livros
@@ -10,4 +11,4 @@ def test_post_livro():
     ApiUtils.payload_parse(payload)
     response = PostLivrariaClient.post_livros(payload)
     PostLivrariaClient.validate_response(response, 201)
-    
+    ApiUtils.validate_json_schema(response, post_schema)
